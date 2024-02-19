90baf9311da37538b6d3efc6d60cfe1c78766fce
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2024-02-19 16:19:31 -0500

    Update and create new Frontend requirements

--- frontend/src/componets/Formulario.jsx
+++ frontend/src/componets/Formulario.jsx
@@ -1,88 +1,157 @@
+import { useContext, useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import Mensaje from "./Alertas/Mensaje";
+import JSConfetti from 'js-confetti'
 
 export const Formulario = () => {
+
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti({
+        emojis: ['🐶', '😽', '🐭', '🦜', '🙈', '🐊'],
+     })
+
+    const navigate = useNavigate()
+    const [mensaje, setMensaje] = useState({})
+    const [form, setform] = useState({
+        nombre: "",
+        propietario: "",
+        email: "",
+        celular: "",
+        convencional: "",
+        sintomas: ""
+    })
+
+    const handleChange = (e) => {
+        setform({...form,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const handleSubmit = async(e) => { 
+        e.preventDefault()
+        try {
+            const token = localStorage.getItem('token')
+            const url = `${import.meta.env.VITE_BACKEND_URL}/paciente/registro`
+            const options={
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            await axios.post(url,form,options)
+						setMensaje({ respuesta:"paciente registrado con exito y correo enviado", tipo: true })
+            setTimeout(() => {
+                navigate('/dashboard/listar');
+            }, 3000);
+        } catch (error) {
+						setMensaje({ respuesta: error.response.data.msg, tipo: false })
+            setTimeout(() => {
+                setMensaje({})
+            }, 3000);
+        }
+    }
+
     return (
-        <form>
-
+        
+        <form onSubmit={handleSubmit}>
+            {Object.keys(mensaje).length>0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
             <div>
                 <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
+                    htmlFor='nombre:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre de la mascota: </label>
                 <input
                     id='nombre'
                     type="text"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
+                    placeholder='nombre de la mascota'
                     name='nombre'
+                    onChange={handleChange}
                 />
             </div>
             <div>
                 <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
+                    htmlFor='propietario:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre del propietario: </label>
                 <input
-                    id='nombre'
+                    id='propietario'
                     type="text"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
+                    placeholder='nombre del propietario'
+                    name='propietario'
+                    onChange={handleChange}
                 />
             </div>
             <div>
                 <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
+                    htmlFor='email:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Email: </label>
                 <input
-                    id='nombre'
-                    type="text"
+                    id='email'
+                    type="email"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
+                    placeholder='email del propietario'
+                    name='email'
+                    onChange={handleChange}
                 />
             </div>
             <div>
                 <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
+                    htmlFor='celular:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Celular: </label>
                 <input
-                    id='nombre'
-                    type="text"
+                    id='celular'
+                    type="number"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
+                    placeholder='celular del propietario'
+                    name='celular'
+                    onChange={handleChange}
                 />
             </div>
             <div>
                 <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
+                    htmlFor='convencional:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Convencional: </label>
                 <input
-                    id='nombre'
-                    type="text"
+                    id='convencional'
+                    type="number"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
+                    placeholder='convencional del propietario'
+                    name='convencional'
+                    onChange={handleChange}
                 />
             </div>
-
             <div>
                 <label
-                    htmlFor='detalles'
-                    className='text-gray-700 uppercase font-bold text-sm'>Detalles: </label>
+                    htmlFor='Salida:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Fecha de salida: </label>
+                <input
+                    id='salida'
+                    type="date"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='salida'
+                    name='salida'
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='sintomas:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Síntomas: </label>
                 <textarea
-                    id='detalles'
+                    id='sintomas'
                     type="text"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    name='detalles'
+                    placeholder='Ingrese los síntomas de la mascota'
+                    name='sintomas'
+                    onChange={handleChange}
                 />
             </div>
 
             <input
                 type="submit"
-                className='bg-gray-600 w-full p-3 
-        text-slate-300 uppercase font-bold rounded-lg 
-        hover:bg-gray-900 cursor-pointer transition-all'
+                className='bg-gray-600 w-full p-3 text-slate-300 uppercase font-bold rounded-lg hover:bg-gray-900 cursor-pointer transition-all'
                 value='Registrar' />
-
         </form>
     )
 }
