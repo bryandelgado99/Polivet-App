5c3438ef55071c2315ae66d47c821c8d30388746
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2024-02-19 14:26:07 -0500

    Modified DB String

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,17 +2,16 @@
 //Importar mongoose
 import mongoose from 'mongoose'
 
+const Compass_URI = "mongodb://0.0.0.0:27017/vet"
 
-// Pertimitir que solo los campos definidos en el Schema sean almacenados
-// enn la BDD
+// Pertimitir que solo los campos definidos en el Schema sean almacenados en la BDD
 mongoose.set('strictQuery', true)
-
 
 // Crear una función llamada connection()
 const connection = async()=>{
     try {
         // Establecer al conexión con la BDD
-        const {connection} = await mongoose.connect(process.env.MONGODB_URI)
+        const {connection} = await mongoose.connect(process.env.MONGODB_URI || Compass_URI)
         
         // Presentar la conexión en consola 
         console.log(`Database is connected on ${connection.host} - ${connection.port}`)
