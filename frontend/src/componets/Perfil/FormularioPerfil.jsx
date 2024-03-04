18083a67a9695ab1e6d5f554db8d72ea40f59114
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2024-03-04 00:37:11 -0500

    Agregado perfil del veterinario y visualización de datos

--- frontend/src/componets/Perfil/FormularioPerfil.jsx
+++ frontend/src/componets/Perfil/FormularioPerfil.jsx
@@ -1,123 +1,131 @@
-import React, { useState } from 'react';
+import { useContext, useState } from "react"
+import AuthContext from "../../context/AuthProvider"
+import Mensaje from "../Alertas/Mensaje"
 
 const FormularioPerfil = () => {
-  // Estado para almacenar los valores del formulario
-  const [formData, setFormData] = useState({
-    nombre1: '',
-    nombre2: '',
-    nombre3: '',
-    detalles: ''
-  });
+    const [mensaje, setMensaje] = useState({})
+    const {auth,actualizarPerfil} = useContext(AuthContext)
+    const [form, setform] = useState({
+				id: auth._id,
+        nombre: auth.nombre || "",
+        apellido:auth.apellido || "",
+        direccion: auth.direccion || "",
+        telefono: auth.telefono || "",
+        email: auth.email || ""
+    })
 
-  // Estado para manejar errores de validación
-  const [errors, setErrors] = useState({});
-
-  // Función para manejar cambios en los campos del formulario
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  // Función para manejar el envío del formulario
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Realizar validaciones antes de enviar el formulario
-    const validationErrors = validateForm(formData);
-    if (Object.keys(validationErrors).length === 0) {
-      // Enviar el formulario si no hay errores de validación
-      console.log('Formulario enviado:', formData);
-    } else {
-      // Actualizar el estado de errores si hay errores de validación
-      setErrors(validationErrors);
+      setform({
+          ...form,
+          [e.target.name]: e.target.value
+      })
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (Object.values(form).includes(""))
+    {
+        setMensaje({ respuesta: "Todos los campos deben ser ingresados", tipo: false })
+            setTimeout(() => {
+                setMensaje({})
+            }, 3000);
+        return
     }
-  };
 
-  // Función para realizar validaciones de formulario
-  const validateForm = (data) => {
-    const errors = {};
-    // Validar que los nombres no estén vacíos
-    if (!data.nombre1.trim()) {
-      errors.nombre1 = 'El nombre 1 es requerido';
+    const resultado = await actualizarPerfil(form)
+      setMensaje(resultado)
+      setTimeout(() => {
+          setMensaje({})
+      }, 3000);
     }
-    if (!data.nombre2.trim()) {
-      errors.nombre2 = 'El nombre 2 es requerido';
-    }
-    if (!data.nombre3.trim()) {
-      errors.nombre3 = 'El nombre 3 es requerido';
-    }
-    // Puedes agregar más validaciones según tus necesidades
+    
+    return (
+      <form onSubmit={handleSubmit}>
+      {Object.keys(mensaje).length > 0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
+            <div>
+                <label
+                    htmlFor='nombre'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
+                <input
+                    id='nombre'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='nombre'
+                    name='nombre'
+                    value={form.nombre}
+                    onChange={handleChange}
+                />
+            </div>
 
-    return errors;
-  };
+            <div>
+                <label
+                    htmlFor='apellido'
+                    className='text-gray-700 uppercase font-bold text-sm'>Apellido: </label>
+                <input
+                    id='apellido'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='apellido'
+                    name='apellido'
+                    value={form.apellido}
+                    onChange={handleChange}
+                />
+            </div>
 
-  return (
-    <form onSubmit={handleSubmit}>
+            <div>
+                <label
+                    htmlFor='direccion'
+                    className='text-gray-700 uppercase font-bold text-sm'>Dirección: </label>
+                <input
+                    id='direccion'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='direccion'
+                    name='direccion'
+                    value={form.direccion}
+                    onChange={handleChange}
+                />
+            </div>
 
-      <div>
-        <label htmlFor='nombre1' className='text-gray-700 uppercase font-bold text-sm'>Nombre 1: </label>
-        <input
-          id='nombre1'
-          type="text"
-          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.nombre1 && 'border-red-500'}`}
-          placeholder='Nombre 1'
-          name='nombre1'
-          value={formData.nombre1}
-          onChange={handleChange}
-        />
-        {errors.nombre1 && <p className="text-red-500 text-xs italic">{errors.nombre1}</p>}
-      </div>
-      <div>
-        <label htmlFor='nombre2' className='text-gray-700 uppercase font-bold text-sm'>Nombre 2: </label>
-        <input
-          id='nombre2'
-          type="text"
-          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.nombre2 && 'border-red-500'}`}
-          placeholder='Nombre 2'
-          name='nombre2'
-          value={formData.nombre2}
-          onChange={handleChange}
-        />
-        {errors.nombre2 && <p className="text-red-500 text-xs italic">{errors.nombre2}</p>}
-      </div>
-      <div>
-        <label htmlFor='nombre3' className='text-gray-700 uppercase font-bold text-sm'>Nombre 3: </label>
-        <input
-          id='nombre3'
-          type="text"
-          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5 ${errors.nombre3 && 'border-red-500'}`}
-          placeholder='Nombre 3'
-          name='nombre3'
-          value={formData.nombre3}
-          onChange={handleChange}
-        />
-        {errors.nombre3 && <p className="text-red-500 text-xs italic">{errors.nombre3}</p>}
-      </div>
+            <div>
+                <label
+                    htmlFor='telefono'
+                    className='text-gray-700 uppercase font-bold text-sm'>Teléfono: </label>
+                <input
+                    id='ditelefonoreccion'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='telefono'
+                    name='telefono'
+                    value={form.telefono}
+                    onChange={handleChange}
+                />
+            </div>
 
-      <div>
-        <label htmlFor='detalles' className='text-gray-700 uppercase font-bold text-sm'>Detalles: </label>
-        <textarea
-          id='detalles'
-          type="text"
-          className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-          name='detalles'
-          value={formData.detalles}
-          onChange={handleChange}
-        />
-      </div>
+            <div>
+                <label
+                    htmlFor='email'
+                    className='text-gray-700 uppercase font-bold text-sm'>Email: </label>
+                <input
+                    id='email'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='email'
+                    name='email'
+                    value={form.email}
+                    onChange={handleChange}
+                />
+            </div>
 
-      <input
-        type="submit"
-        className='bg-gray-800 w-full p-3 
+            <input
+                type="submit"
+                className='bg-gray-800 w-full p-3 
         text-slate-300 uppercase font-bold rounded-lg 
         hover:bg-gray-600 cursor-pointer transition-all'
-        value='Actualizar'
-      />
+                value='Actualizar' />
 
-    </form>
-  );
-};
+        </form>
+    )
+}
 
-export default FormularioPerfil;
+export default FormularioPerfil
--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -21,6 +21,27 @@ const AuthProvider = ({ children }) => {
             console.log(error);
         }
     }
+
+    const actualizarPerfil = async(datos) => {
+        const token = localStorage.getItem('token')
+        try {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/${datos.id}`
+            const options = {
+                headers: {
+                    method: 'PUT',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            const respuesta = await axios.put(url, datos, options)
+            perfil(token)
+            return {respuesta:respuesta.data.msg,tipo:true}
+        } catch (error) {
+            return {respuesta:error.response.data.msg,tipo:false}
+        }
+}
+
+
     useEffect(() => {
         const token = localStorage.getItem('token')
         if(token)
@@ -33,7 +54,8 @@ const AuthProvider = ({ children }) => {
         <AuthContext.Provider value={
             {
                 auth,
-                setAuth              
+                setAuth,
+                actualizarPerfil         
             }
         }>
             {children}
