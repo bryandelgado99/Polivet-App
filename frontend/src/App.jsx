3f93784943d0c319e898ab7b58b64b1c879a5725
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2024-03-04 11:18:09 -0500

    Mejora de página para restablecer password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Auth from './layout/Auth'
 import Login from './paginas/Login'
 import { LandinPage } from './paginas/LandinPage'
 import { Register } from './paginas/Register'
-import { Forgot } from './paginas/Forgot'
 import { NotFound } from './paginas/NotFound'
 import Dashboard from './layout/Dashboard'
 import Listar from './paginas/Listar'
@@ -17,7 +16,7 @@ import Restablecer from './paginas/Restablecer'
 import { AuthProvider } from './context/AuthProvider'
 import { PrivateRoute } from './routes/PrivateRoutes'
 import { TratamientosProvider } from './context/TratamientosProvider'
-
+import { Forgot } from './paginas/Forgot'
 
 function App() {
   return (
@@ -31,7 +30,7 @@ function App() {
           <Route path='/' element={<Auth/>}>
             <Route path='login' element={<Login/>}/>
             <Route path='register' element={<Register/>}/>
-            <Route path='forgot/:id' element={<Forgot/>}/>
+            <Route path='forgot' element={<Forgot/>}/>
             <Route path='confirmar/:token' element={<Confirmar/>}/>
             <Route path='recuperar-password/:token' element={<Restablecer/>}/>
             <Route path='*' element={<NotFound />} />
